a909826cefc4f7f1be651e7882712ce84273c20d
Anton Rufino <anton.vongola@gmail.com>
2016-05-04 10:46:19 +0800

    Create window.

--- src/AVS.java
+++ src/AVS.java
@@ -2,9 +2,15 @@ package avs;
 
 import avs.models.*;
 
-public class AVS {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class AVS extends JFrame {
     public static void main(String[] args) {
         // Demo goes here
+        new AVS();
+
         Energy energy = new Energy(0, 0);
         Zombie zombie = new Zombie(0, 0, 10);
         Thread energyThread = new Thread(energy);
@@ -12,5 +18,14 @@ public class AVS {
 
         energyThread.start();
         zombiethread.start();
+    }
+
+    public AVS() {
+        super("Acads vs. Students");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setPreferredSize(new Dimension(800, 600));
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
     }
 }
