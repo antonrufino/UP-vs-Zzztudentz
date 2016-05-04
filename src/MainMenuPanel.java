fc6237bfd92c04efa62603362b62c5cc8981e335
Anton Rufino <anton.vongola@gmail.com>
2016-05-04 18:19:06 +0800

    Create MainMenuPanel

---
+++ src/MainMenuPanel.java
@@ -0,0 +1,11 @@
+package avs.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainMenuPanel extends JPanel {
+    public MainMenuPanel() {
+        this.setBackground(Color.GREEN);
+    }
+}
