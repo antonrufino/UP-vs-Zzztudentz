a0a7c3ebcce4aecbdf35362260e195e0e597bed7
Anton Rufino <anton.vongola@gmail.com>
2016-05-06 10:54:37 +0800

    Create prototype of f game ui

--- src/AVS.java
+++ src/AVS.java
@@ -11,13 +11,5 @@ public class AVS {
     public static void main(String[] args) {
         // Demo goes here
         new MainFrame();
-
-        Energy energy = new Energy(0, 0);
-        Zombie zombie = new Zombie(0, 0, 10);
-        Thread energyThread = new Thread(energy);
-        Thread zombiethread = new Thread(zombie);
-
-        energyThread.start();
-        zombiethread.start();
     }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,9 +3,69 @@ package avs.ui;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 
 public class GamePanel extends JPanel {
     public GamePanel() {
-        this.setBackground(Color.RED);
+        this.setLayout(new BorderLayout());
+        this.setOpaque(false);
+
+        JPanel topPanel = new JPanel(new BorderLayout());
+        topPanel.setOpaque(false);
+        topPanel.add(createPlantsPanel(), BorderLayout.WEST);
+        topPanel.add(createProgressPanel(), BorderLayout.EAST);
+
+        this.add(topPanel, BorderLayout.NORTH);
+        this.add(createCanvasPanel(), BorderLayout.CENTER);
+    }
+
+    private JPanel createPlantsPanel() {
+        JPanel plantsPanel = new JPanel();
+
+        plantsPanel.setOpaque(false);
+
+        plantsPanel.add(new JLabel("Energy: 150"));
+        plantsPanel.add(new JButton("Kopiko"));
+        plantsPanel.add(new JButton("Kwek Kwek"));
+        plantsPanel.add(new JButton("Banga"));
+        plantsPanel.add(new JButton("TC7"));
+
+        return plantsPanel;
+    }
+
+    private JPanel createProgressPanel() {
+        JPanel progressPanel = new JPanel();
+
+        progressPanel.setOpaque(false);
+
+        progressPanel.add(new JLabel("Progress"));
+        progressPanel.add(new JButton("Menu"));
+
+        return progressPanel;
+    }
+
+    private JPanel createCanvasPanel() {
+        JPanel canvasPanel = new JPanel();
+        canvasPanel.setBackground(null);
+        canvasPanel.setOpaque(false);
+        canvasPanel.add(new JLabel("Kunwari may shits dito."));
+        return canvasPanel;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        super.paintComponent(g);
+
+        try {
+            BufferedImage bg = ImageIO.read(
+                new File("assets/background.png"));
+            g.drawImage(bg, 0, 0, null);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
