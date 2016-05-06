8610646e2d0508eb00eee6fe5984352ca7e6a419
Anton Rufino <anton.vongola@gmail.com>
2016-05-06 10:18:05 +0800

    Change window size

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -32,7 +32,7 @@ public class MainFrame extends JFrame {
         cardPanel.add("settings", new SettingsPanel());
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setPreferredSize(new Dimension(800, 600));
+        this.setPreferredSize(new Dimension(1280, 720));
         this.pack();
         this.setLocationRelativeTo(null);
 
