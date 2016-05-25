c82fe9ac905204749485f067438a6aeb16c11947
Anton Rufino <anton.vongola@gmail.com>
2016-05-26 00:17:28 +0800

    Implement grid.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,7 @@
 package avs.ui;
 
+import avs.models.Grid;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -7,11 +9,17 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Rectangle;
 
 public class GamePanel extends JPanel {
     private static BufferedImage bg;
+    Grid grid;
 
     public GamePanel() {
+        grid = new Grid();
+
         this.setLayout(new BorderLayout());
         this.setOpaque(false);
 
@@ -32,9 +40,12 @@ public class GamePanel extends JPanel {
 
         new Thread(new Runnable() {
             public void run() {
-                while (!progressBarPanel.isDone()) {
+                while (true) {
                     try {
-                        progressBarPanel.update();
+                        if (!progressBarPanel.isDone()) {
+                            progressBarPanel.update();
+                        }
+
                         repaint();
                         progressBarPanel.repaint();
                         Thread.sleep(100);
@@ -57,23 +68,46 @@ public class GamePanel extends JPanel {
         JPanel canvasPanel = new JPanel();
         canvasPanel.setBackground(null);
         canvasPanel.setOpaque(false);
-        
+
         return canvasPanel;
     }
 
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         super.paintComponent(g);
 
         try {
-            g.drawImage(GamePanel.bg, 0, 0,
+            g2d.drawImage(GamePanel.bg, 0, 0,
                 this.getWidth(), this.getHeight(), null);
+
+            g2d.setColor(new Color(1, 68, 33));
+
+            Point p = this.getMousePosition();
+            if (p != null) {
+                createHiglightThread(p, g2d).start();
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    private Thread createHiglightThread(final Point p, final Graphics2D g2d) {
+        return new Thread() {
+            public void run() {
+                for (int i = 0; i < Grid.ROWS; ++i) {
+                    for (int j = 0; j < Grid.COLS; ++j) {
+                        Rectangle rect = grid.getRectangle(i, j);
+                        if (rect.contains(p)) {
+                            g2d.draw(rect);
+                        };
+                    }
+                }
+            }
+        };
     }
 
     public static class AssetLoader implements Runnable {
---
+++ src/Grid.java
@@ -0,0 +1,29 @@
+package avs.models;
+
+import java.awt.Rectangle;
+
+public class Grid {
+    public static final int ROWS = 5;
+    public static final int COLS = 9;
+    private final int TILE_WIDTH = 113;
+    private final int TILE_HEIGHT = 109;
+    private final int BUS_OFFSET = 139;
+    private final int SIDEWALK_OFFSET = 157;
+
+    private Rectangle[][] tiles = new Rectangle[ROWS][COLS];
+    private Plant[][] plants = new Plant[ROWS][COLS];
+
+    public Grid() {
+        for (int i = 0; i < ROWS; ++i) {
+            for (int j = 0; j < COLS; ++j) {
+                int x = BUS_OFFSET + j * TILE_WIDTH;
+                int y = SIDEWALK_OFFSET + i * TILE_HEIGHT;
+                tiles[i][j] = new Rectangle(x, y, TILE_WIDTH, TILE_HEIGHT);
+            }
+        }
+    }
+
+    public Rectangle getRectangle(int row, int col) {
+        return tiles[row][col];
+    }
+}
