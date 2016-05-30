d2510d6c1001f6abd87819882308d698363b5d34
Anton Rufino <anton.vongola@gmail.com>
2016-05-30 14:12:37 +0800

    Create new constructors.

--- src/Plant.java
+++ src/Plant.java
@@ -7,17 +7,20 @@ import java.awt.*;
 
 import javax.swing.*;
 
-public abstract class Plant {
+public abstract class Plant extends Entity {
+    protected BufferedImage[] animation;
 	private boolean isAlive;
     protected int cost;
 
-    public Plant(Textures texx) {
+    public Plant(int w, int h, int cost, Textures texx) {
         super(texx);
+        this.cost = cost;
         this.isAlive = true;
     }
 
-	public Plant(double x, double y, Textures texx){
-		super(x, y, texx);
+	public Plant(double x, double y, int w, int h, int cost, Textures texx){
+		super(x, y, w, h, texx);
+        this.cost = cost;
 		this.isAlive = true;
 	}
 
