e7cf3c769e9509944a31133ed647edf16dd5919c
Anton Rufino <anton.vongola@gmail.com>
2016-05-31 18:51:58 +0800

    Remove cheat energy.

--- src/Game.java
+++ src/Game.java
@@ -38,7 +38,7 @@ public class Game{
     }
 
     public void init() {
-        this.energy = 9000;
+        this.energy = 0;
         this.grid = new Grid();
         this.pendingPlant = null;
         this.pendingButton = null;
