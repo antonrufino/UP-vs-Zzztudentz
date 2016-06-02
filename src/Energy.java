bf65c0fd147ff0512195c0b1f1b3979e4a516a39
Anton Rufino <anton.vongola@gmail.com>
2016-06-02 22:02:01 +0800

    Implement energy life span.

--- src/Energy.java
+++ src/Energy.java
@@ -7,16 +7,20 @@ import java.awt.*;
 
 import javax.swing.*;
 
-public class Energy extends Entity{
+public class Energy extends Entity implements Runnable {
 	private BufferedImage energyImg;
     private double targetY;	//where will it land
-    int amount;
+    private int amount;
+    private boolean alive;
+    private final int LIFE_SPAN = 9000;
+    private Thread thread;
 
 	public Energy(double x, double y, double targetY, int amount, Textures texx){
 		super(x,y,48,75,texx);
         this.targetY = targetY;
         this.amount = amount;
 		energyImg = texx.getEnergy();
+        this.alive = true;
 	}
 
 	public void tick(){
@@ -29,5 +33,33 @@ public class Energy extends Entity{
 
     public int getAmount() {
         return this.amount;
+    }
+
+    public boolean isAlive() {
+        return this.alive;
+    }
+
+    public void start() {
+        this.thread = new Thread(this);
+        this.thread.start();
+    }
+
+    private void stop() {
+        this.thread.interrupt();
+    }
+
+    public void kill() {
+        this.alive = false;
+        this.stop();
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(LIFE_SPAN);
+            this.alive = false;
+        } catch (InterruptedException e) {
+            return;
+        }
     }
 }
